3a27ef96dc2b0ba102d4f64c3b40dafe2506fd73
changjiale <1056430312@qq.com>
2019-03-23 15:23:39 +0800

    晚上项目说明

--- setting.py
+++ setting.py
@@ -4,11 +4,11 @@
 friend_dict={
     'alarm_time': '9:30',
     #微信名称(或备注) ps:不能是微信号，不能不能
-    'wechat_name': 'xxx',
+    'wechat_name': '木子',
     #所在城市,
     'city_name': '洛阳',
     # 相识时间
-    'start_date': '2019-1-26',
+    'start_date': '2010-9-1',
     # 短句的最后留言
     'sweet_words': '                  来自最爱你的我',
 }
